8a05f1b25390da35b670adf088d48675728524f1
danb <dan@bryanlabs.net>
2023-02-24 19:21:48 -0500

    updated genesis with vals

--- testnet/grand-1/accept-gentx.sh
+++ testnet/grand-1/accept-gentx.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -eux
 # quick script to import all the gentx files with vested balances into the chain genesis.
 
 # Each Validator will recieve 1000000ustake
@@ -6,10 +7,10 @@ amount='1000000ustake'
 
 # This is the epoch time when it will unlock
 unlock='531800807734'
-noble_template_path='/home/danb/code/github/strangelove-ventures/noble-networks/testnet/grand-1/genesis.json'
+noble_template_path='/home/danb/code/github/strangelove-ventures/noble-networks/testnet/grand-1/gentx/genesis.json'
 
 # create noble env
-nobled init default
+# nobled init default
 
 # custom noble template
 cp $noble_template_path ~/.noble/config/genesis.json
@@ -26,7 +27,7 @@ do
 done
 
 # Collect the gentx
-nobled collect-gentxs --gentx-dir .
+# nobled collect-gentxs --gentx-dir .
 
 # Create the chain genesis.
-cp ~/.noble/config/genesis.json genesis-grand-1.json
+cp ~/.noble/config/genesis.json ../genesis.json
