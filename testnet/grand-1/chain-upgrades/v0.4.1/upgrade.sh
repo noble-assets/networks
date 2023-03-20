58ad306297c7927770fabdad57a870a4565ec2c3
danb <dan@bryanlabs.net>
2023-03-20 13:17:06 -0400

    v0.4.1 proposal artifact

--- testnet/grand-1/chain-upgrades/v0.4.1/upgrade.sh
+++ testnet/grand-1/chain-upgrades/v0.4.1/upgrade.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 nobled tx upgrade software-upgrade \
-'v0.4.0' \
+'v0.4.1' \
 --upgrade-height 302000 \
---upgrade-info 'https://github.com/strangelove-ventures/noble-networks/blob/main/testnet/grand-1/chain-upgrades/v0.4.0/README.md' \
+--upgrade-info 'https://github.com/strangelove-ventures/noble-networks/blob/main/testnet/grand-1/chain-upgrades/v0.4.1/README.md' \
 --from noble10uu75g7zl0gnzt0wz46htgqnl5ml27dnthcztx
