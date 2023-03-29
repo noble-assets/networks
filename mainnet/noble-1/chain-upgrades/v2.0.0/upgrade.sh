a83d2b0b91e70ae7682ad49dc3df0ce19ea41c80
danb <dan.bryan@strangelove.ventures>
2023-03-29 16:22:15 -0400

    Update upgrade.sh

--- mainnet/noble-1/chain-upgrades/v2.0.0/upgrade.sh
+++ mainnet/noble-1/chain-upgrades/v2.0.0/upgrade.sh
@@ -2,5 +2,5 @@
 nobled tx upgrade software-upgrade \
 'neon' \
 --upgrade-height 119000 \
---upgrade-info 'https://github.com/strangelove-ventures/noble-networks/blob/main/testnet/noble-1/chain-upgrades/v2.0.0/README.md' \
+--upgrade-info 'https://github.com/strangelove-ventures/noble-networks/blob/main/mainnet/noble-1/chain-upgrades/v2.0.0/README.md' \
 --from noble1tq944l2tgxugwvu74yke37yt7pa27p8467rxg5
