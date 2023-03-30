f2707341819227d4c433660a79349cf62997eb75
danb <dan@bryanlabs.net>
2023-03-30 10:53:35 -0400

    multisig rework

--- mainnet/noble-1/chain-upgrades/v2.0.0/upgrade.sh
+++ mainnet/noble-1/chain-upgrades/v2.0.0/upgrade.sh
@@ -3,4 +3,5 @@ nobled tx upgrade software-upgrade \
 'neon' \
 --upgrade-height 119000 \
 --upgrade-info 'https://github.com/strangelove-ventures/noble-networks/blob/main/mainnet/noble-1/chain-upgrades/v2.0.0/README.md' \
---from noble1tq944l2tgxugwvu74yke37yt7pa27p8467rxg5
+--from noble1tq944l2tgxugwvu74yke37yt7pa27p8467rxg5 \
+--generate-only > neon-upgrade-unsigned.json
