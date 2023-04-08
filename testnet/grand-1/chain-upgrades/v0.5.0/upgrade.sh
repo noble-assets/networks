a7536ff11ed5c10383869dfccc9399301f2861f5
danbryan <dan@bryanlabs.net>
2023-04-08 15:17:44 -0400

    updated plane info

--- testnet/grand-1/chain-upgrades/v0.5.0/upgrade.sh
+++ testnet/grand-1/chain-upgrades/v0.5.0/upgrade.sh
@@ -3,5 +3,4 @@ nobled tx upgrade software-upgrade \
 'raydon' \
 --upgrade-height 645000 \
 --upgrade-info '{"binaries":{"linux/amd64":"https://github.com/strangelove-ventures/noble/releases/download/v0.5.0/nobled-v0.5.0-linux-amd64?checksum=sha256:949e84d7600f8520d4eecfd2aa67c81f6b25a3c48f8a66fdc58ba1ee19f9c91a"},"urls":{"docs":"https://github.com/strangelove-ventures/noble-networks/blob/main/testnet/grand-1/chain-upgrades/v0.5.0/README.md"}}' \
---from noble10uu75g7zl0gnzt0wz46htgqnl5ml27dnthcztx \
---generate-only > raydon-upgrade-unsigned.json
+--from noble10uu75g7zl0gnzt0wz46htgqnl5ml27dnthcztx
